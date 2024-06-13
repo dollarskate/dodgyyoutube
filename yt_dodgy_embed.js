b89410c1b011b65f245e6871fa3522ebdc021a6d
dollar skate <xan2116@googlemail.com>
2024-06-13 23:42:13 +0100

    Update yt_dodgy_embed.js

--- yt_dodgy_embed.js
+++ yt_dodgy_embed.js
@@ -3,18 +3,28 @@
 // @namespace   w/e
 // @description good
 // @include         https://*.youtube.com/*
-// @version     2
+// @version     3
 // @downloadURL   https://raw.githubusercontent.com/dollarskate/dodgyyoutube/main/yt_dodgy_embed.js
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
 function diabolicallyBad()
 {
+    var replacedElement = jQuery("iframe#myverycoolembededvid");
+    var url = "https://www.youtube.com/embed/"+(new URLSearchParams(window.location.search)).get("v")+"?autoplay=1"
+
+    if (replacedElement.length > 0)
+    {
+        if (replacedElement[0].src != url) //we've navigated away
+        {
+            replacedElement.remove()
+        }
+    }
     if (location.pathname=="/watch")
     {
-        var containerElement = $("div#player-container-inner");
-        var videoElement1 = $("div#player-container");
-        var videoElement2 = $("video.video-stream.html5-main-video");
-        var replacedElement = $("#myverycoolembededvid");
+        var containerElement = jQuery("div#player-container-inner");
+        var videoElement1 = jQuery("div#player-container");
+        var videoElement2 = jQuery("video.video-stream.html5-main-video");
 
 
         if (videoElement1.length > 0)
@@ -31,7 +41,6 @@ function diabolicallyBad()
         {
             //replace with iframe containing embeded video
             console.log("my goodness, what are you doing?");
-            var url = "https://www.youtube.com/embed/"+(new URLSearchParams(window.location.search)).get("v")+"?autoplay=1"
             //just sort of randomly tried some css until it worked
             containerElement.append('<iframe id="myverycoolembededvid" src="'+url+'" style="height: -webkit-fill-available; width: -webkit-fill-available;top: 0; position: absolute;"></iframe>');
         }
